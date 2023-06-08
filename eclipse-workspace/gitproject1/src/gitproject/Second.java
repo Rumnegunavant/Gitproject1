30aff16a972d065e0ee9d21ff65561df0d528cff
Rumnegunavant <rumnegunavant@gmail.com>
2023-06-08 15:10:42 +0530

    Adding second class

---
+++ eclipse-workspace/gitproject1/src/gitproject/Second.java
@@ -0,0 +1,18 @@
+package gitproject;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "second";
+		
+		if(s.equals("second")) {
+			System.out.println("SCRRrrrrr ");
+		}else {
+			System.out.println("dob dob dob iss");
+		}
+
+	}
+
+}
